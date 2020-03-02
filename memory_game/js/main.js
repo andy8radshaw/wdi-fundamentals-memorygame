433b1bdee3f907501703c2747c71ff2b24666964
Andy Bradshaw <andy@Andys-MacBook-Pro.local>
2020-03-02 11:24:20 +0000

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
+
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
