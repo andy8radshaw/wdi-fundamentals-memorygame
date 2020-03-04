41c65e33e2b0a61e37966a9793dd87364b32742d
Andy Bradshaw <andy@Andys-MacBook-Pro.local>
2020-03-04 11:29:55 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,20 @@
-console.log("Up and running!");
+let cards = ["queen", "queen", "king", "king"];
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+let cardsInPlay = [];
 
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardTwo);
+let cardOne = cards[1];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped king")
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!")
+}
+else {
+    alert("Sorry Try again")
+}
