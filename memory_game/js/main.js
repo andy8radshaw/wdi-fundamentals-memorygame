f260b8da81c4fde2e2694849705081ff4f2b8215
Andy Bradshaw <andy@Andys-MacBook-Pro.local>
2020-03-05 20:52:17 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,19 +2,21 @@ let cards = ["queen", "queen", "king", "king"];
 
 let cardsInPlay = [];
 
-let cardOne = cards[1];
-
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-
-let cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-console.log("User flipped king")
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!")
+function checkForMatch() {
+    if (cardsInPlay.length === 2) {
+      if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+      } else {
+        console.log("Sorry, try again.");
+      }
+    }
 }
-else {
-    alert("Sorry Try again")
+
+function flipCard(cardId){
+    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId])
+    checkForMatch();
 }
+
+flipCard(0);
+flipCard(2);
