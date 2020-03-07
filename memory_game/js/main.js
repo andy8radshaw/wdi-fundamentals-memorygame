5413febcbd6d76ce8a91410069935f9240a94951
Andy Bradshaw <andy@Andys-MacBook-Pro.local>
2020-03-07 12:44:10 +0000

    created more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,23 +23,34 @@ let cards = [
 
 let cardsInPlay = [];
 
-function checkForMatch() {
-    if (cardsInPlay.length === 2) {
-      if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
-      } else {
-        alert("Sorry, try again.");
-      }
-    }
+function createBoard() {
+  for (let i = 0; i < cards.length; i++) {
+    let cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard)
+    document.getElementById('game-board').appendChild(cardElement)
+  }
 }
 
-function flipCard(cardId){
-    cardsInPlay.push(cards[cardId].rank);
-    console.log("User flipped " + cards[cardId].rank);
+function checkForMatch() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } 
+    else {
+      alert("Sorry, try again.");
+    }
+  }
+}
+
+function flipCard(){
+  let cardId = this.getAttribute('data-id');
+    console.log("User flipped the " + cards[cardId].rank + " of " + cards[cardId].suit);
     console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
+    cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute('src', cards[cardId].cardImage);
     checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
